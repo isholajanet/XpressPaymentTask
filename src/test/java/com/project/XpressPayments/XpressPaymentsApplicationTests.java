049ea044299c49b56b082d15fac010fea378e5cb
Janet Ishola <janetishola2010@gmail.com>
2024-01-08 15:26:55 +0100

    test

--- src/test/java/com/project/XpressPayments/XpressPaymentsApplicationTests.java
+++ src/test/java/com/project/XpressPayments/XpressPaymentsApplicationTests.java
@@ -1,7 +1,11 @@
 package com.project.XpressPayments;
 
+import com.project.XpressPayments.service.PaymentServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class XpressPaymentsApplicationTests {
